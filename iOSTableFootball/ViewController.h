137b9f679f0fbd512b8b3d717764b66b33f037dc
Magdalena Sarzynska <magda2609@gmail.com>
2015-07-22 14:24:27 +0200

    Initial Commit

---
+++ iOSTableFootball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTableFootball
+//
+//  Created by Magdalena Sarzynska on 22/07/15.
+//  Copyright (c) 2015 Kainos Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSTableFootball/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSTableFootball
+//
+//  Created by Magdalena Sarzynska on 22/07/15.
+//  Copyright (c) 2015 Kainos Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
